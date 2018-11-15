ae0ad115ec86ac73b730a6fa08032ebfe40afa71
Pedro <pedro.teix@ua.pt>
2018-11-15 23:10:14 +0000

    Added logging (how to in the beginning)

--- student.py
+++ student.py
@@ -1,12 +1,25 @@
 from tree_search import *
 import random
-
+import logging
 
 #$ PORT=80 SERVER=pacman-aulas.ws.atnog.av.it.pt python client.py
 # to kill server: fuser 8000/tcp
 
+# for debug purposes
 debug = True
 
+
+# logs are written to file logger.log after the client is closed
+# possible messages: debug, info, warning, error, critical 
+# how to use: logging.typeOfMessage('message')
+
+if (debug):
+    logger = logging.getLogger('student_logger')
+    logger_format = '[%(lineno)s - %(funcName)20s() - %(levelname)s]\n %(message)s\n'
+    #logger_format = '%(levelname)s:\t%(message)' # simpler format
+
+    # currently writing over the logger file, change filemode to a to append
+    logging.basicConfig(format=logger_format, filename='logger.log', filemode='w', level=logging.DEBUG)
 
 class Pacman_agent():
     """Creates the PACMAN agent that analyses the given 'Map' and 'state'
@@ -25,6 +38,10 @@ class Pacman_agent():
 
 
     def __init__(self, map_): 
+        if debug:
+            logger.warning('\n\n\n ========================== NEW EXECUTION ==========================\n')
+            logger.debug('CREATING PACMAN AGENT\n')
+
         # static info from mapa.py Map
         self.map_ = map_
         self.pathways = self.create_pathways_list()
@@ -33,7 +50,7 @@ class Pacman_agent():
         self.corr_adjacencies =self.create_corridor_adjacencies(self.corridors, self.crossroads)
 
         if debug:
-            print('CREATED PACMAN AGENT')
+            logger.debug('CREATED PACMAN AGENT')
 
         
 
@@ -48,15 +65,15 @@ class Pacman_agent():
         Returns: the key corresponding to the next move of PACMAN
         """
 
-        #print("\nEnergy size is : " + str(len(state['energy'])) + "\n")
+        #logger.debug(nt("\nEnergy size is : " + str(len(state['energy'])) + "\n")
         # create a vector for every element in the game
         # every element points pacman teh next move to get to it
         vectors = []
-        #print(state['energy'])
+        #logger.debug(nt(state['energy'])
         
         pac_pos = (state['pacman'][0], state['pacman'][1])
         # if debug:
-        #     print("\t pacman is in position " + str(pac_pos))
+        #     logger.debug("\t pacman is in position " + str(pac_pos))
 
         ex, ey = self.get_vector(nodes_to_search=state['energy'], pac_pos=pac_pos)
         #(gx, gy) = self.get_vector(state['ghosts'], pac_pos)
@@ -86,10 +103,10 @@ class Pacman_agent():
             elif vec_x < 0 and vec_y > 0:
                 key = random.choice('as')
             elif vec_x == 0:
-                print("There is a problem not solved yet in this line of code!")
+                logger.warning("There is a problem not solved yet in this line of code!")
         
         if debug:
-            print('The key is: ' + str(key))
+            logger.debug('The key is: ' + str(key))
 
 
         # x, y = state['pacman']
@@ -117,9 +134,9 @@ class Pacman_agent():
         next_pos = []
         vectors = []
         # if debug:
-        #     print("***********************************************************")
-        #     print('\t get vector was called! ')
-        #     print("***********************************************************")
+        #     logger.debug("***********************************************************")
+        #     logger.debug('\t get vector was called! ')
+        #     logger.debug("***********************************************************")
 
         # convert list to dictionary with zero weight for each element
         weight_dict = { (x,y):1 for [x,y] in nodes_to_search }
@@ -127,24 +144,24 @@ class Pacman_agent():
         for (x,y) in nodes_to_search:
 
             # if debug:
-                # print("#######################################################")
-                # print('\t calculating vector for pos: ' + str((x,y)))
-                # print("#######################################################")
+                # logger.debug("#######################################################")
+                # logger.debug('\t calculating vector for pos: ' + str((x,y)))
+                # logger.debug("#######################################################")
         
             # if debug:
-            #     print("\t cycle  for position " + str((x,y)))
+            #     logger.debug("\t cycle  for position " + str((x,y)))
 
             # search the path
             # if debug:
-            #     print("SearchDomain being called to create")
+            #     logger.debug("SearchDomain being called to create")
             domain = Pathways(self.adjacencies)
 
             # if debug:
-            #     print("SearchProblem " + str(i) + " being called to create")
+            #     logger.debug("SearchProblem " + str(i) + " being called to create")
             my_prob = SearchProblem(domain,(x,y),pac_pos)
             
             # if debug:
-            #     print("SearchTree " + str(i) + " being called to create")
+            #     logger.debug("SearchTree " + str(i) + " being called to create")
             my_tree = SearchTree(my_prob, weight_dict, self.strategy)
             
             next_result = my_tree.search()
@@ -155,15 +172,15 @@ class Pacman_agent():
             else:
                 next_pos += [((x,y), pac_pos, 0)]
 
-            #print((x,y))
-            #print(next_result)
+            #logger.debug((x,y))
+            #logger.debug(next_result)
             
-            #print("\t search " + str(i) + " was completed!")
+            #logger.debug("\t search " + str(i) + " was completed!")
 
             # if debug:
-            #     print('\t Calculating next move for position: ' + str((x,y)))
+            #     logger.debug('\t Calculating next move for position: ' + str((x,y)))
 
-        #print(next_pos)
+        #logger.debug(next_pos)
 
         for i in range(len(next_pos)):
             if next_pos[i][1] != pac_pos:
@@ -189,14 +206,14 @@ class Pacman_agent():
                     dir = ( 0 , (-(1/next_pos[i][2])) )
                 vectors += [dir]
 
-                print(str(next_pos[i][0]) + " : vector is: " + str(dir))
+                logger.debug(str(next_pos[i][0]) + " : vector is: " + str(dir))
             
             # if debug:
-            #     print("#######################################################")
-            #     print('\t Vector is ' + str(dir))
-            #     print("#######################################################")
+            #     logger.debug("#######################################################")
+            #     logger.debug('\t Vector is ' + str(dir))
+            #     logger.debug("#######################################################")
 
-        #print(weight_dict)
+        #logger.debug(weight_dict)
 
 
 
@@ -207,13 +224,13 @@ class Pacman_agent():
             vec_x += x
             vec_y += y
         
-        #print("\npacman is in position " + str(pac_pos[0], pac_pos[1]))
-        #print('Sum of all vectors is: ' + str(vec_x) + ', ' + str(vec_y) + "\n")
+        #logger.debug("\npacman is in position " + str(pac_pos[0], pac_pos[1]))
+        #logger.debug('Sum of all vectors is: ' + str(vec_x) + ', ' + str(vec_y) + "\n")
 
         if debug:
-            print("#######################################################")
-            print('\t Vector is ' + str((vec_x, vec_y)))
-            print("#######################################################")
+            logger.debug("#######################################################")
+            logger.debug('\t Vector is ' + str((vec_x, vec_y)))
+            logger.debug("#######################################################")
 
         return [vec_x, vec_y]
 
@@ -271,10 +288,10 @@ class Pacman_agent():
         list_   -- a list
         n       -- number of elements per combination
         """
-        print("#######################################################")
-        print('\t ' + string + ' is: ')
-        print("#######################################################")
-        print(var)
+        logger.debug("#######################################################")
+        logger.debug('\t ' + string + ' is: ')
+        logger.debug("#######################################################")
+        logger.debug(var)
 
 
 ################################################################################
